364eb27a9c3aa568ec848ed3a979af92cf1bb751
Eric Jarvi <ejarvi@users.noreply.github.com>
2017-10-13 15:28:57 -0700

    bash script for cli 2.0 validation

---
+++ validate.sh
@@ -0,0 +1,153 @@
+#!/bin/bash
+set -x
+
+# make sure azure cli is logged in 
+if az account show | grep -m 1 "login"; then 
+  exit 1
+fi
+
+# make sure azure cli is installed
+if ! [ -x "$(command -v az)" ]; then
+  echo 'Error: Azure CLI 2.0 is not installed.  See: https://docs.microsoft.com/en-us/cli/azure/install-azure-cli' >&2
+  exit 1
+fi
+
+# make sure jq is installed (sudo apt-get install jq) 
+if ! [ -x "$(command -v jq)" ]; then
+  echo 'Error: jq package is not installed. On Ubuntu use "sudo apt-get install jq" or for other options see: https://stedolan.github.io/jq/download/' >&2
+  exit 1
+fi
+
+if [[ ( -z "$1") || ( -z "$2") ]]
+then
+    echo "usage: validate.sh [image] [location]
+
+    [image] may take the form of an alias, URN, resource ID, or URI. 
+
+        Example Alias:
+            CentOS, CoreOS, Debian, openSUSE, RHEL, SLES, UbuntuLTS, etc..
+        Example URN: 
+            MicrosoftWindowsServer:WindowsServer:2012-R2-Datacenter:latest
+        Example Custom Image Resource ID or Name: 
+            /subscriptions/subscription-id/resourceGroups/MyResourceGroup/providers/Microsoft.Compute/images/MyImage
+        Example URI: 
+            http://<storageAccount>.blob.core.windows.net/vhds/osdiskimage.vhd
+
+    [location] is the Azure region to use for all resources in the validation test
+        Example regions: 
+            westus, centralus, eastus, etc..
+        To get a list of regions use:  
+            az account list-locations
+    "
+    exit 1
+fi
+
+ADE_IMAGE="$1"
+ADE_LOCATION="$2"
+
+# initialize globals for use during the test
+ADE_SUBSCRIPTION_ID="`az account show | jq -r '.id'`"
+ADE_PREFIX="ade`cat /dev/urandom | tr -dc 'a-z' | fold -w 6 | head -n 1`";
+ADE_RG="${ADE_PREFIX}rg"
+ADE_VNET="${ADE_PREFIX}vnet"
+ADE_SUBNET="${ADE_PREFIX}subnet"
+ADE_PUBIP="${ADE_PREFIX}pubip"
+ADE_NSG="${ADE_PREFIX}nsg"
+ADE_NIC="${ADE_PREFIX}nic"
+ADE_VM="${ADE_PREFIX}vm"
+ADE_ADAPP_SECRET="$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)"
+ADE_ADAPP_CPS_NAME="CN=www.contoso.com"
+ADE_ADAPP_CERT_NAME="${ADE_PREFIX}cert"
+ADE_ADAPP_NAME="${ADE_PREFIX}adapp"
+ADE_ADAPP_URI="https://localhost/${ADE_ADAPP_NAME}"
+ADE_KV_NAME="${ADE_PREFIX}kv"
+ADE_KEK_NAME="${ADE_PREFIX}kek"
+
+# create resource group which will contain all test resources (except for ad application and service principal)
+az group create --name ${ADE_RG} --location ${ADE_LOCATION}
+
+# create ad application 
+az ad app create --display-name $ADE_ADAPP_NAME --homepage $ADE_ADAPP_URI --identifier-uris $ADE_ADAPP_URI --password $ADE_ADAPP_SECRET
+ADE_ADSP_APPID="`az ad app list --display-name ${ADE_ADAPP_NAME} | jq -r '.[0] | .appId'`"
+
+# create service principal for ad application 
+az ad sp create --id "${ADE_ADSP_APPID}"
+ADE_ADSP_OID="`az ad sp list --display-name ${ADE_ADAPP_NAME} | jq -r '.[0] | .objectId'`"
+
+# create role assignment for ad app
+az role assignment create --assignee $ADE_ADSP_OID --role Reader --scope "/subscriptions/${ADE_SUBSCRIPTION_ID}/"
+
+# create keyvault and set policy
+az keyvault create --name ${ADE_KV_NAME} --resource-group ${ADE_RG} --location ${ADE_LOCATION}
+ADE_KV_URI="`az keyvault show --name ${ADE_KV_NAME} --resource-group ${ADE_RG} | jq -r '.properties.vaultUri'`"
+ADE_KV_ID="`az keyvault show --name ${ADE_KV_NAME} --resource-group ${ADE_RG} | jq -r '.id'`"
+az keyvault set-policy --name "${ADE_KV_NAME}" --resource-group "${ADE_RG}" --spn "${ADE_ADSP_APPID}" --key-permissions "wrapKey" --secret-permissions "set"
+az keyvault update --name "${ADE_KV_NAME}" --resource-group "${ADE_RG}" --enabled-for-deployment true --enabled-for-disk-encryption true
+
+# create key encryption key
+az keyvault key create --vault-name ${ADE_KV_NAME} --name ${ADE_KEK_NAME} --protection Software 
+ADE_KEK_ID="${ADE_KV_ID}"
+ADE_KEK_URI="`az keyvault key show --name ${ADE_KEK_NAME} --vault-name ${ADE_KV_NAME} | jq -r '.key.kid'`"
+
+# create network resources
+az network vnet create --resource-group ${ADE_RG} --name ${ADE_VNET} --subnet-name ${ADE_SUBNET}
+az network public-ip create --resource-group ${ADE_RG} --name ${ADE_PUBIP}
+az network nsg create --resource-group ${ADE_RG} --name ${ADE_NSG}
+az network nic create --resource-group ${ADE_RG} --name ${ADE_NIC} --vnet-name ${ADE_VNET} --subnet ${ADE_SUBNET} --network-security-group ${ADE_NSG} --public-ip-address ${ADE_PUBIP}
+
+# create virtual machine 
+az vm create --resource-group ${ADE_RG} --name ${ADE_VM} --nics ${ADE_NIC} --image ${ADE_IMAGE} --generate-ssh-keys
+az vm open-port --port 22 --resource-group ${ADE_RG} --name ${ADE_VM}
+
+# encrypt virtual machine 
+az encryption enable --name "${ADE_VM}" --resource-group "${ADE_RG}" --aad-client-id "${ADE_ADSP_APPID}" --aad-client-secret "${ADE_ADAPP_SECRET}" --disk-encryption-keyvault "${ADE_KV_ID}" --key-encryption-key "${ADE_KEK_URI}" --key-encryption-keyvault "${ADE_KEK_ID}" --volume-type ALL
+
+# check status once every 5 minutes for 6 hours
+SLEEP_CYCLES=0
+MAX_SLEEP=72
+until az vm encryption show --name "${ADE_VM}" --resource-group "${ADE_RG}" | grep -m 1 "VMRestartPending" || [ $SLEEP_CYCLES -eq $MAX_SLEEP ]; do
+   date
+   # display current progress while waiting for the VMRestartPending message
+   az vm encryption show --name "${ADE_VM}" --resource-group "${ADE_RG}" | grep -m 1 "progressMessage"
+   sleep 5m
+   (( SLEEP_CYCLES++ ))
+done
+
+print_delete_instructions()
+{
+    echo "To delete all test resources:
+    az group delete -n ${ADE_RG} --no-wait
+    az ad app delete --id ${ADE_ADSP_APPID}"
+}
+
+if [ $SLEEP_CYCLES -eq $MAX_SLEEP ]
+then
+    echo "default timeout threshold expired - OS disk encryption took more than 6 hours"
+    print_delete_instructions
+    exit 1
+fi
+
+# VMRestartPending message displayed, so restart the vm 
+az vm restart --name "${ADE_VM}" --resource-group "${ADE_RG}"
+
+# check status once every 30 seconds for 10 minutes  (after restart, the extension needs time to start up and mount the newly encrypted disk)
+SLEEP_CYCLES=0
+MAX_SLEEP=20
+until az vm encryption show --name "${ADE_VM}" --resource-group "${ADE_RG}" | grep -m 1 "succeeded" || [ $SLEEP_CYCLES -eq $MAX_SLEEP ]; do
+   date
+   # display current progress while waiting for the succeeded message
+   az vm encryption show --name "${ADE_VM}" --resource-group "${ADE_RG}" | grep -m 1 "osDisk"
+   sleep 30
+   (( SLEEP_CYCLES++ ))
+done
+
+if [ $SLEEP_CYCLES -eq $MAX_SLEEP ]
+then
+    echo "timeout threshold expired - OS disk encryption success message not observed after restart"
+    print_delete_instructions
+    exit 1
+fi
+
+echo "To delete all test resources:
+    az group delete -n ${ADE_RG} --no-wait
+    az ad app delete --id ${ADE_ADSP_APPID}"
